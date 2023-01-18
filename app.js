e04afe4de8ee8b1aaf77f729bde5ec2ceffae1f0
siddhant3c <csiddhant274@gmail.com>
2023-01-18 15:38:28 +0530

    completed html structure, working on css and js

---
+++ app.js
@@ -0,0 +1,20 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add('fadeIn');
+    });
+  };
+
+  startGame();
+//   updateScore();
+};
+
+game();
